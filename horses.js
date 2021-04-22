c75feded3865ef998b7487d3fa2f44cc060c524e
Maggie McCausland <maggiemccausland@hotmail.com>
2021-04-22 01:23:55 -0600

    Update horses.js

--- horses.js
+++ horses.js
@@ -57,7 +57,7 @@ body.appendChild(button);
 // 3. Add event handler
 button.addEventListener ("click", function() {
 //   location.href = ['myList[i].link'];
-      location.href = myList[i].link;
+location.href = myList[i]['link'];
 
 
 });
